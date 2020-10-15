c520b2ff6291ffbccf5e1dcaf298c927f1f0fa9d
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-15 17:54:47 +0530

    Added : Code to calculate Daily Wage

---
+++ UC2EmployeeWage.java
@@ -0,0 +1,23 @@
+package com.employeeWageforMultipleCompanies;
+
+import java.util.Random;
+
+public class UC2EmployeeWage {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program");
+
+			int WAGE_PER_HR = 20;
+			int FULL_DAY_HRS = 8;
+			double daily_Wage;
+			Random rand = new Random();
+			int random = rand.nextInt(2);
+			if (random == 1) {
+				daily_Wage = WAGE_PER_HR * FULL_DAY_HRS;
+				System.out.println("Employee-Present" + " " + "daily_Wage =" + daily_Wage);
+			} else
+				System.out.println("Employee-Absent" + " " + "Daily Wage = 0");
+		}
+
+	
+}

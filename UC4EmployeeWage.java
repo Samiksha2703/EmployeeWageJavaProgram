d3f4c2f46472f0a6057adbac81263289d2010b2d
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-15 18:05:30 +0530

    Added : Code for Daily Wage with Switch Case

---
+++ UC4EmployeeWage.java
@@ -0,0 +1,32 @@
+package com.employeeWageforMultipleCompanies;
+
+import java.util.Random;
+
+public class UC4EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		int WAGE_PER_HR = 20;
+		int FULL_DAY_HRS = 8;
+		int PART_TIME_HRS = 4;
+		double daily_Wage;
+		Random rand = new Random();
+		int random = rand.nextInt(3);
+		switch (random) {
+		case 1:
+			daily_Wage = WAGE_PER_HR * FULL_DAY_HRS;
+			System.out.println("Employee - Present" + " " + "daily_Wage = " + daily_Wage);
+			break;
+		case 2:
+			daily_Wage = WAGE_PER_HR * PART_TIME_HRS;
+			System.out.println("Employee - Present" + " " + "daily_Wage = " + daily_Wage);
+			break;
+		case 0:
+			System.out.println("Employee - Absent" + " " + "Daily Wage = 0");
+			break;
+		}
+	}
+
+	
+}

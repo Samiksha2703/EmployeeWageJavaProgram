c60bcae1f14e67b5b47baae2c1f7f1c85ab7b52c
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-15 17:48:57 +0530

    Added : Code to check the attendance

---
+++ UC1EmployeeWage.java
@@ -0,0 +1,18 @@
+package com.employeeWageforMultipleCompanies;
+
+import java.util.Random;
+
+public class UC1EmployeeWage {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program");
+			Random rand = new Random();
+			int random = rand.nextInt(2);
+			if (random == 1)
+				System.out.println("Employee-Absent");
+			else
+				System.out.println("Employee-Present");
+		}
+	
+}
+	
